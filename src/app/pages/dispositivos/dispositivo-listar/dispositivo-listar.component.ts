61243faf38908a87f5c2a54fa21f619551f35968
Alexandre Jales <alexandre.jales@hotmail.com>
2021-06-13 20:32:59 -0300

    Ajustes

--- src/app/pages/dispositivos/dispositivo-listar/dispositivo-listar.component.ts
+++ src/app/pages/dispositivos/dispositivo-listar/dispositivo-listar.component.ts
@@ -1,37 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { DispositivoForm } from 'src/app/model/form/dispositivo-form';
 import { DispositivoService } from 'src/app/service/dispositivo.service';
 import { ToastService } from 'src/app/service/toast.service';
 
 @Component({
-	selector: 'app-dispositivo-listar',
-	templateUrl: './dispositivo-listar.component.html',
-	styleUrls: ['./dispositivo-listar.component.scss']
+    selector: 'app-dispositivo-listar',
+    templateUrl: './dispositivo-listar.component.html',
+    styleUrls: ['./dispositivo-listar.component.scss']
 })
 export class DispositivoListarComponent implements OnInit {
 
-	displayedColumns: string[] = ['item', 'nome', 'filialRazaoSocial', 'dataExpiracao', 'bloqueado', 'acao'];
-	dados: DispositivoForm[] = [];
-	index: number = 0;
+    displayedColumns: string[] = [];
+    columnsLarger: string[] = ['item', 'nome', 'filialRazaoSocial', 'dataExpiracao', 'bloqueado', 'acao'];
+    columnsSmall: string[] = ['item', 'nome', 'acao'];
+    dados: DispositivoForm[] = [];
+    index = 0;
 
-	constructor(private dispositivoService: DispositivoService, private toastService: ToastService) { }
+    constructor(private dispositivoService: DispositivoService, private toastService: ToastService) { }
 
-	ngOnInit(): void {
-		this.onFindAll();
-	}
+    ngOnInit(): void {
+        this.onScrennResize();
+        this.onFindAll();
+    }
 
-	onFindAll() {
-		this.dispositivoService.findAll().subscribe(
-			{
-				next: data => {
-					this.dados = data;
-				},
-				error: error => {
-					//Imprime erro da exception da API
-					this.toastService.openAlertSnackBar("Erro ao carregar os dados");
-				}
-			}
-		);
-	}
+    @HostListener('window:resize', ['$event'])
+    onScrennResize(): void {
+        if (window.innerWidth > 576) {
+            this.displayedColumns = this.columnsLarger;
+        } else {
+            this.displayedColumns = this.columnsSmall;
+        }
+    }
 
+    onFindAll(): void {
+        this.dispositivoService.findAll().subscribe(
+            {
+                next: data => {
+                    this.dados = data;
+                },
+                error: error => {
+                    // Imprime erro da exception da API
+                    this.toastService.openAlertSnackBar('Erro ao carregar os dados');
+                }
+            }
+        );
+    }
 }
